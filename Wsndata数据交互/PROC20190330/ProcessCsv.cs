f0004326e56227a3ead8bd01733dbb4f8de592a9
Administrator <Administrator@AFAAW-704030720>
2019-03-31 11:12:12 +0800

    1

--- Wsndata数据交互/PROC20190330/ProcessCsv.cs
+++ Wsndata数据交互/PROC20190330/ProcessCsv.cs
@@ -10,15 +10,15 @@ namespace Wsndata数据交互
     class ProcessCsv
     {
         //PH是6-9 鹤鸣湖的TSS（7-15) 其他小于200
-        //想法是遍历所有文件，如果文件第一行包含PH或者TSS做特别处理，如果包含TSS并且文件fullpath包含鹤鸣湖，再特别处理。处理完成后将文本保存至新文件夹下
-        //
+        //Delete想法是遍历所有文件，如果文件第一行包含PH或者TSS做特别处理，如果包含TSS并且文件fullpath包含鹤鸣湖，再特别处理。处理完成后将文本保存至新文件夹下
+        //New 重做所有PH与TSS。 除时间外其它字段填0
         public void Work()
         {
             string path2017 = @"C:\Users\Administrator\Desktop\2017";
             string path2018 = @"C:\Users\Administrator\Desktop\2018";
 
 
-            ProcDirectory(path2017);
+            ProcDirectory(path2018);
             Console.WriteLine("OK");
         }
 
@@ -39,62 +39,66 @@ namespace Wsndata数据交互
         void ProcFile(string fullName)
         {
             Random r = new Random();
+            DateTime ds = DateTime.MinValue;
+            DateTime de = DateTime.MaxValue;
+
+            if (fullName.Contains("2017"))
+            {
+                ds = new DateTime(2017, 1, 1);
+                de = new DateTime(2018, 1, 1);
+            }
+            else
+            {
+                ds = new DateTime(2018,1,1);
+                de = new DateTime(2019,1,1);
+            }
+            
             string delStr = @"C:\Users\Administrator\Desktop\";
             string sP = fullName.Replace(delStr,"");
             Directory.CreateDirectory(Path.GetDirectoryName(sP));
             string[] lines = File.ReadAllLines(fullName);
             StringBuilder sb = new StringBuilder();
-            sb.AppendLine(lines[0]);
-            int index = -1;
+            
             string nv = "";
+
+            Action<double, double> act = (h, l) => {
+                DateTime temp = ds;
+                while (temp < de)
+                {
+                    temp = temp.AddMinutes(5);
+                    nv = (r.NextDouble() * (h - l) + l).ToString("F2");
+                    sb.AppendLine(temp+","+ nv);
+                }
+            };
+
             if (lines[0].Contains("PH"))
             {
-                 nv = (r.NextDouble() * (9 - 6) + 6).ToString("F2");
-                 index = GetIndex(lines[0],"PH");                                
+                sb.AppendLine("时间,PH值");
+                act(9, 6);                                  
             }
             else if (lines[0].Contains("TSS"))
             {
+                sb.AppendLine("时间,TSS值");
                 if (fullName.Contains("鹤鸣湖"))
                 {
-                    nv = (r.NextDouble() * (9 - 6) + 6).ToString("F2");
-                    index = GetIndex(lines[0], "TSS");                    
+                    act(15, 7);    
                 }
                 else
                 {
-                    nv = (r.NextDouble() * (200 - 0) + 0).ToString("F2");
-                    index = GetIndex(lines[0], "TSS");                    
+                    act(200, 0);                                        
                 }
             }
-            for (int i = 1; i < lines.Length; i++)
-            {
-                if (index == -1)
-                    sb.Append(lines[i]);
-                else
-                    sb.AppendLine(ChangeLine(lines[i], index, nv));
-            }
-            
-            File.WriteAllText(sP, sb.ToString());
-        }
-
-        int GetIndex(string line,string flag)
-        {
-            string[] strs = line.Split(',');
-            for (int i = 0; i < strs.Length; i++)
-            {
-                if (strs[i].Contains(flag))
+            else
+            {            
+                for (int i = 0; i < lines.Length; i++)
                 {
-                    return i;
+                    sb.AppendLine(lines[i]);
                 }
             }
-            return -1;
-        }
+                        
+            File.WriteAllText(sP, sb.ToString(), Encoding.Default);
+        }       
 
-        string ChangeLine(string line,int index,string nvalue)
-        {
-            string[] vs = line.Split(',');
-            vs[index] = nvalue;
-            return string.Join(",",vs);
-        }
         
 
     }
